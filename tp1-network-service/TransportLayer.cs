f229b3aab0f34fd0167d678d5982e64b11a554ea
Antoine Guérin <90921460+AntGuerin23@users.noreply.github.com>
2024-09-24 15:54:42 -0400

    Feat: Added parameters in Layer.cs

--- tp1-network-service/TransportLayer.cs
+++ tp1-network-service/TransportLayer.cs
@@ -1,13 +1,15 @@
+using tp1_network_service.Messages;
+
 namespace tp1_network_service;
 
-public class TransportLayer : Layer
+internal class TransportLayer : Layer
 {
-    public override void StartListening()
+    public override void StartListening(string filesPath)
     {
         
     }
 
-    internal override void HandleNewMessage()
+    internal override void HandleNewMessage(Message message)
     {
         
     }
